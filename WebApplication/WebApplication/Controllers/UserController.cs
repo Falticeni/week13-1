ce206e58f0ea56844e426ca13135a842c12e0b98
Ciocan Robert Vasile <ciocanrobertvasile91@gmail.com>
2019-07-22 20:30:01 +0300

    Week 13

--- WebApplication/WebApplication/Controllers/UserController.cs
+++ WebApplication/WebApplication/Controllers/UserController.cs
@@ -9,10 +9,11 @@ namespace Students.Web.Controllers
     {
         private static readonly List<UserViewModel> Users = new List<UserViewModel>
         {
-            new UserViewModel { Id = 1, Email = "dan@yahoo.com", UserName = "dan" },
-            new UserViewModel { Id = 2, Email = "andrei@yahoo.com", UserName = "andrei" },
-            new UserViewModel { Id = 3, Email = "vlad@yahoo.com", UserName = "vlad" },
-        }; 
+            new UserViewModel { Id = 1, Email = "dan@yahoo.com", UserName = "Dan", Adresa = new Address{ City="Iasi", Street="Nicolina"} },
+            new UserViewModel { Id = 2, Email = "andrei@yahoo.com", UserName = "Andrei", Adresa = new Address{ City="Cluj Napoca", Street="Napoca"} },
+            new UserViewModel { Id = 3, Email = "vlad@yahoo.com", UserName = "Vlad", Adresa = new Address{ City = "Brasov", Street="Victriei"} },
+            new UserViewModel { Id = 4, Email = "lucian@yahoo.com", UserName = "Lucian", Adresa = new Address{ City = "Bucuresti", Street="Ploii"} },
+        };
 
         [HttpGet]
         public ActionResult Index()
@@ -24,7 +25,6 @@ namespace Students.Web.Controllers
         public ActionResult Details(int id)
         {
             var model = Users.FirstOrDefault(x => x.Id == id);
-
             return View(model);
         }
 
@@ -34,17 +34,44 @@ namespace Students.Web.Controllers
             return View();
         }
 
+        //public ActionResult AddNew()
+        //{
+        //    var username = Request.Form["username"];
+        //    var email = Request.Form["email"];
+
+        //    var user = new UserViewModel { Email = email, UserName = username, Id = Users.Count + 1 };
+
+        //    Users.Add(user);
+
+        //    return RedirectToAction("Index");
+        //}
+
         [HttpPost]
-        public ActionResult AddNew()
+        public ActionResult Add(UserViewModel user)
         {
-            var username = Request.Form["username"];
-            var email = Request.Form["email"];
-
-            var user = new UserViewModel { Email = email, UserName = username, Id = Users.Count + 1 };
-
+            var count = Users.Count();
+            user.Id = count + 1;
             Users.Add(user);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            //Users.Remove(Users.FirstOrDefault(x => x.Id == id));
+
+            return View(Users.FirstOrDefault(x => x.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete()
+        {
+            var id = Request.Form["Id"];
+
+            Users.Remove(Users.FirstOrDefault(x => x.Id == int.Parse(id)));
 
             return RedirectToAction("Index");
         }
     }
 }
+
--- WebApplication/WebApplication/Models/UserViewModel.cs
+++ WebApplication/WebApplication/Models/UserViewModel.cs
@@ -1,11 +1,30 @@
-﻿namespace Students.Web.Models
+﻿using System.ComponentModel;
+
+
+namespace Students.Web.Models
 {
     public class UserViewModel
     {
+        [DisplayName("Nume Utilizator")]
         public int Id { get; set; }
+
+        [DisplayName("Nume Utilizator")]
 
         public string UserName { get; set; }
 
+        [DisplayName("Adresa Email")]
         public string Email { get; set; }
+
+        [DisplayName("Address")]
+        public Address Adresa { get; set; }
+    }
+
+    public class Address
+    {
+        [DisplayName("Strada")]
+        public string Street { get; set; }
+
+        [DisplayName("Oras")]
+        public string City { get; set; }
     }
 }
